0872e50ca0b0ecf5abb241a967fe8f837ac16f90
Igusaya <igusaya2014@gmail.com>
2019-12-08 21:58:38 +0900

    implement event new component

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,10 +1,26 @@
 import _ from 'lodash'
-import { READ_EVENTS } from '../actions'
+import { READ_EVENTS
+       , READ_EVENT
+       , CREATE_EVENT
+       , UPDATE_EVENT
+       , DELETE_EVENT
+} from '../actions'
 
 export default (events = {}, action) => {
     switch (action.type) {
         case READ_EVENTS:
             return _.mapKeys(action.response.data, 'id')
+        case READ_EVENT:
+        case CREATE_EVENT:
+        case UPDATE_EVENT:
+            const data = action.response.data
+            return { ...events, [data.id]: data}
+        case DELETE_EVENT:
+            // delete演算子はオブジェクトからプロパティを削除する
+            delete events[action.id]
+            // スプレット演算子（...）を使用することにより
+            // 新しいメモリ空間上のupdateされたオブジェクト情報を返却
+            return { ...events }
         default:
             return events
     }
